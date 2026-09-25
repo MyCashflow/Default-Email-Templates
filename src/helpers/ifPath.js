7cb8edc82812b8d2901efaeee36ad66e9b251840
miko <miko@pulse247.info>
2026-09-25 10:53:21 +0300

    Refactor ifPath helper to simplify path matching logic

--- src/helpers/ifPath.js
+++ src/helpers/ifPath.js
@@ -1,14 +1,5 @@
-function toRegExp(pattern) {
-	const source = pattern
-		.replace(/[.+^${}()|[\]\\]/g, '\\$&')
-		.replace(/\*\*/g, '\u0000')
-		.replace(/\*/g, '[^/]*')
-		.replace(/\u0000/g, '.*');
-	return new RegExp(`^${source}$`);
-}
 module.exports = function(...args) {
 	const options = args.pop();
 	const current = this._page || options?.data?.root?._page || options?.data?.root?.page;
-	const matched = args.some(pattern => toRegExp(pattern).test(current));
-	return matched ? options.fn(this) : options.inverse(this);
+	return args.includes(current) ? options.fn(this) : options.inverse(this);
 };
